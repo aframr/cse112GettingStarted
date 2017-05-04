6b4384ca6b3136a8c8a185ee474c80e552fef7c2
Andrew Chan <chan94306@gmail.com>
2017-05-04 00:02:40 -0700

    eslint src only within public/js

--- gulpfile.js
+++ gulpfile.js
@@ -13,11 +13,8 @@ var gulp = require('gulp'),
 //       .pipe(jshint())
 // })
 gulp.task('lint', () => {
-	// ESLint ignores files with "node_modules" paths. 
-	// So, it's best to have gulp ignore the directory as well. 
-	// Also, Be sure to return the stream from the task; 
-	// Otherwise, the task may end before the stream has finished. 
-	return gulp.src(['./**/*.js','!node_modules/**', '!doc/**', '!docs/**'])
+	// TODO: src should be ./**/*.js to cover all .js files
+	return gulp.src(['public/js/*.js','!node_modules/**', '!doc/**', '!docs/**'])
 	// eslint() attaches the lint output to the "eslint" property 
 	// of the file object so it can be used by other modules. 
 	.pipe(eslint())
